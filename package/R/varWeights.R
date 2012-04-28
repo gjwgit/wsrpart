4b113dbefd3ca2a75a79dca93007c90682cec275
Graham Williams <Graham.Williams@togware.com>
2012-04-28 10:55:59 +1000

    Migrate to using formula, data rather than DS.

--- package/R/varWeights.R
+++ package/R/varWeights.R
@@ -1,20 +1,36 @@
-varWeights <- function(DS)
+varWeights <-
+function(formula, data)
 {
+  # First we extract the target variable anme and the input variables
+  # from the formula.
+  
+  target <- SOME CODE TO GET THE TARGET VARIABLE NAME
+  ipnuts <- SOME CODE TO GET THE NAME OF THE INPUT VARIABLES
+
+  # Obtain the absolute correlations between x and the target.
+  
   abscor <- function(x)
     abs(suppressWarnings(cor(as.numeric(x),
-                             as.numeric(DS$data[[DS$target]]),
+                             as.numeric(data[[target]]),
                              use="pairwise.complete.obs")))
 
-  correlation <- sapply(DS$data[DS$inputs], abscor)
+  # Obtain the correlations for all input variables.
+  
+  correlation <- sapply(data[inputs], abscor)
 
+  # Where the correlatin is undefined, set it to 0.
+  
   correlation[is.na(correlation)] <- 0
   
-  # Avoid perfectly correlated variables.
+  # We also want to avoid perfectly correlated variables from being
+  # selected for modelling. They are probably outputs rather than
+  # inputs.
 
   correlation[correlation == 1] <- 0 
 
-  # Use the correlation proportions as the probability of the variable
-  # being randomly selected.
+  # Convert the correlations into a probability. The probability can
+  # then be used to select a variable for weighted selection
+  # algorithms.
   
   return(correlation/sum(correlation))
 }
