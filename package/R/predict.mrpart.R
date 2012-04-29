5533ac102933e2c72f217ed1b8cd424d4195eeba
Graham Williams <Graham.Williams@togware.com>
2012-04-29 20:25:02 +1000

    Updates from Zhalama.

--- package/R/predict.mrpart.R
+++ package/R/predict.mrpart.R
@@ -1,30 +1,46 @@
 predict.mrpart <-
-  function(object, newdata, type="class")
+function(object, newdata, type=c("class", "prob", "votes"), ...)
 {
-  scores <- sapply(object, function(m) predict(m$model,newdata,type="class"))
+  if (!inherits(object, "mrpart")) 
+    stop("Not a legitimate multile rpart object")
 
-  scorelist<-data.frame(id=rep(row.names(scores),times=ncol(scores)),
-                        row.names=NULL,class=as.vector(scores))
+  scores <- sapply(object, function(m) predict(m$model, newdata, type="class"))
+
+  scorelist<-data.frame(id=rep(row.names(scores), times=ncol(scores)),
+                        row.names=NULL, class=as.vector(scores))
+  
   votes <- table(scorelist)
 
-  # votes is a table which can not be operated,so we need to convert
-  # votes into a matrix-----scoremat
+  # The variable votes is a table which can not be operated on easily,
+  # so we need to convert votes into a matrix---scoremat.
 
   scoremat <- matrix(as.vector(votes), nrow=nrow(scores))
   row.names(scoremat) <- row.names(scores)
   colnames(scoremat) <- levels(scorelist$class)
-  classlab <- apply(scoremat, 1, function(x)names(which.max(x)))
 
-  #get prob
+  # Build the class list. Ensure the result is a vector and the values
+  # are a factor. This needs to mimic what rpart does. But how is that
+  # information lost from the predict above to here? Is the use of
+  # "names" below that strips the factor?
+  
+  classlab <- as.vector(apply(scoremat, 1, function(x) names(which.max(x))))
+  names(classlab) <- row.names(newdata)
+  classlab <- as.factor(classlab)
+
+  # Get the probabilities.
 
   prob <- t(apply(scoremat, 1, function(x)x/sum(x)))
 
-  #return result
+  # Return the result.
   
-  allResult <- list(class = as.data.frame(classlab), prob = prob,
-                    votes = votes)
-  if (type == "class") return(as.data.frame(classlab))
-  else if (type == "prob") return(prob)
-  else if (type == "votes") return(votes)
-#  else return(allResult)
+  if (type == "class")
+    return(classlab)
+  else if (type == "prob")
+    return(prob)
+  else if (type == "votes")
+    return(votes)
+  else
+    return(list(class = classlab,
+                prob = prob,
+                votes = votes))
 }
