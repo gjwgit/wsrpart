c165425ae09e99c54a8c857f046620b59e93fb3a
Graham Williams <Graham.Williams@togware.com>
2012-04-29 20:23:33 +1000

    Change to formula, data rather than DS

--- package/R/wsrpart.R
+++ package/R/wsrpart.R
@@ -1,31 +1,44 @@
-wsrpart <- function(DS, nvars=trunc(log(DS$ninputs + 1, 2)), ...)
+wsrpart <-
+function(formula, data, nvars, ...)
 {
   require(rpart)
+
+  # Extract the variables from the formula.
+
+  vars <- as.character(attr(terms(model.frame(formula, data)), "variables"))[-1L]
+  target <- vars[[1]]
+  inputs <- vars[-1]
+
+  # How many vars to select.
+
+  if (missing(nvars)) nvars <- trunc(log(length(inputs) + 1, 2))
   
   # Build the new training set each time we call the function as in
   # bagging and random forests. We sample, with replacement, from the
   # dataset container's training set.
   
-  tr <- sample(DS$train, length(DS$train), replace=TRUE)
-  oob <- setdiff(DS$train, tr)
+   tr <- sample(row.names(data), length(row.names(data)), replace=TRUE)
+   oob <- setdiff(row.names(data), tr)
+
+  # Identify the variables from which to build the model.
   
-  vr <- selectVars(DS, nvars)
+  vr <- selectVars(formula, data, nvars)
   
   # Build the decision tree model.
   
-  model <- rpart(formula=DS$form, data=DS$data[tr, c(vr, DS$target)], ...)
+   model <- rpart(formula, data=data[tr, c(vr, target)], ...)
 
   # We should not really have missing target values, but just in
   # case...
   
-  complete <- oob[which(! is.na(DS$data[oob, DS$target]))]
+  complete <- oob[which(! is.na(data[oob, target]))]
 
   # This assumes binary classification.
   
-  oob.error <- sum((predict(model, DS$data[complete,], type="class") !=
-                   DS$data[complete, DS$target])) / length(complete)
+  oob.error <- sum((predict(model, data[complete,], type="class") !=
+                   data[complete, target])) / length(complete)
 
-  oob.scores <- predict(model, DS$data[oob,])
+   oob.scores <- predict(model, data[complete,])
 
   result <- list(model=model,
                  vars.used=vr,
