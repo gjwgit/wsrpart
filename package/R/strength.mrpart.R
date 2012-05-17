8f09e8f5c6b20eb71739f4422db9e87162ad5a44
Graham Williams <Graham.Williams@togware.com>
2012-05-17 20:42:49 +1000

    Simplify by using support functions.

--- package/R/strength.mrpart.R
+++ package/R/strength.mrpart.R
@@ -3,27 +3,10 @@ strength <- function(object, data, formula, ...) UseMethod("strength")
 strength.mrpart <- function(object, data, formula)
 {
 
-  vars <- as.character(attr(terms(model.frame(formula, data)),
-                            "variables"))[-1L]
-  target <- vars[[1]]
+  target <- .getTarget(data, formula)
 
-  
-  oob <- sapply(object, function(m) m$oob.scores)
+  oobmat <- oobmatrix(object, data, formula)
 
-  oobscolist <- list(id=rownames(Reduce(rbind, oob)),
-                     class=apply(Reduce(rbind, oob), 1,
-                       function(x)names(which.max(x))))
-
-  oobscolist$id <- as.integer(oobscolist$id)
-
-  oobvotes <- table(oobscolist)
-
-  oobmat <- matrix(as.vector(oobvotes),
-                   nrow=length(levels(as.factor(oobscolist$id))))
-  row.names(oobmat) <- levels(as.factor(oobscolist$id))
-  colnames(oobmat) <- levels(as.factor(oobscolist$class))
-
-  
   pY <- sapply(row.names(oobmat),
                function(x) oobmat[x, data[x, target]]/sum(oobmat[x,]))
 
